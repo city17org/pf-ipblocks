ebae995fdcc3fe67d70958df49642215f4bf8f8a
Sean Davies <sean@city17.org>
2022-12-11 08:20:41 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+PF-IPBLOCKS(8) - System Manager's Manual
+
+# NAME
+
+**pf-ipblocks** - add a country's IP address blocks to a pf table
+
+# SYNOPSIS
+
+**pf-ipblocks**
+\[**-x**]
+cctld
+
+# DESCRIPTION
+
+**pf-ipblocks**
+is a utility that will download the IP address blocks in CIDR format for the
+country code top-level domain passed as
+*cctld*.
+
+When run without any options the output will then be saved to the following
+location if either the file does not already exist or has changed,
+*/etc/pf-xx.zone*.
+A
+pf(4)
+table called
+*xx.zone*
+will then be either created or updated.
+In both instances,
+*xx*
+is the
+*cctld*
+passed.
+
+Alternatively, the IP address block data can be printed to stdout.
+
+The options are as follows:
+
+**-x**
+
+> Print the IP address block data to stdout and exit.
+
+# EXIT STATUS
+
+The **pf-ipblocks** utility exits&#160;0 on success, and&#160;&gt;0 if an error occurs.
+
+# EXAMPLES
+
+The following
+crontab(5)
+entry runs
+**pf-ipblocks**
+every week to add all the IP addresses of Australia to a
+pf(4)
+table.
+
+	0 22 * * 6 /usr/local/bin/pf-ipblocks au
+
+The following lines can be added to
+pf.conf(5)
+to create a table definition for the Australian IP's and define a filter rule
+to block all traffic coming from addresses listed in the table.
+
+	table <au.zone> persist file "/etc/pf-au.zone"
+	block drop quick on egress from <au.zone> to any
+
+# SEE ALSO
+
+pf(4),
+crontab(5),
+pf.conf(5),
+cron(8),
+pfctl(8),
+
+[https://www.ipdeny.com/ipblocks](https://www.ipdeny.com/ipblocks)
+
+# AUTHORS
+
+Sean Davies &lt;[sean@city17.org](mailto:sean@city17.org)&gt;
---
+++ pf-ipblocks.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+# Copyright (c) 2022 Sean Davies <sean@city17.org>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+set -e
+
+die()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+usage()
+{
+	die "usage: ${0##*/} [-x] cctld"
+}
+
+droproot()
+{
+	local _user=_ftp
+
+	eval su -s /bin/sh "${_user}" -c "'$*'" || exit 1
+}
+
+fetchzonedata()
+{
+	if [ "${xflag}" -eq 0 ] || [ "$(id -u)" -eq 0 ]; then
+		output=$(droproot ftp -MVo - "${url}")
+	else
+		output=$(ftp -MVo - "${url}") || exit 1
+	fi
+
+	if [ -z "${output}" ]; then
+		die "${0##*/}: failed to fetch zone file data"
+	fi
+}
+
+xflag=0
+while getopts x arg; do
+	case ${arg} in
+	x)	xflag=1 ;;
+	*)	usage ;;
+	esac
+done
+shift $((OPTIND - 1))
+[ "$#" -eq 1 ] || usage
+
+case $1 in
+[A-Z][A-Z]) cctld=$(echo "$1" | tr '[:upper:]' '[:lower:]') ;;
+[a-z][a-z]) cctld=$1 ;;
+*) usage ;;
+esac
+url=http://www.ipdeny.com/ipblocks/data/aggregated/${cctld}-aggregated.zone
+
+if [ "${xflag}" -eq 0 ] && [ "$(id -u)" -ne 0 ]; then
+	die "${0##*/}: needs root privileges"
+fi
+
+fetchzonedata
+[ -n "${output}" ] || exit 0
+
+if [ "${xflag}" -eq 0 ]; then
+	outfile=/etc/pf-$1.zone
+	pftable=$1.zone
+
+	if [ ! -f "${outfile}" ] || [ "${output}" != "$(cat "${outfile}")" ]; then
+		echo "${output}" >"${outfile}"
+		pfctl -t "${pftable}" -T replace -f "${outfile}"
+	fi
+else
+	echo "${output}"
+fi
